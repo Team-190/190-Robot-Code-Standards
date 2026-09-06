d65760520c05872b9daaeeeb13e00c3efb76b5e1
ElliotScher <ecscher84@gmail.com>
2026-09-06 16:58:53 -0400

    Add number 5

---
+++ docs/training/xrp_java_course/DATA_DATA_EVERYWHERE.md
@@ -0,0 +1,9 @@
+---
+hide_table_of_contents: true
+---
+
+import GoogleSlidesEmbed from '@site/src/components/GoogleSlidesEmbed';
+
+# Data… Data Everywhere
+
+<GoogleSlidesEmbed url="https://docs.google.com/presentation/d/1z7vZ3qlyRwsCpp9487V8Wx_r6ckKVsSRqVkjYuWkK0Q/edit?usp=sharing" />
--- sidebars.ts
+++ sidebars.ts
@@ -510,7 +510,8 @@ const sidebars: SidebarsConfig = {
         'training/xrp_java_course/XRP_SETUP',
         'training/xrp_java_course/WPILIB_INSTALLATION',
         'training/xrp_java_course/GITTING_STARTED',
-         'training/xrp_java_course/MAKING_IT_MOVE'
+        'training/xrp_java_course/MAKING_IT_MOVE',
+        'training/xrp_java_course/DATA_DATA_EVERYWHERE'
       ],
     },
   ],
